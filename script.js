c755a5a4aa5f60cc4b04abe5700a15cfaba7176b
Nona <yuzbashyan.nonna@gmail.com>
2021-09-11 10:33:22 +0400

    project setup

---
+++ script.js
@@ -0,0 +1,32 @@
+const images = [
+    "images/dice-1.svg",
+    "images/dice-2.svg",
+    "images/dice-3.svg",
+    "images/dice-4.svg",
+    "images/dice-5.svg",
+    "images/dice-6.svg"];
+
+let dice = document.querySelectorAll("img");
+const rollButton = document.getElementById("roll");
+
+function roll() {
+    dice.forEach(function (die) {
+        die.classList.add("shake");        
+    });
+    setTimeout(function () {
+        dice.forEach(function (die){            
+            die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random()* images.length);
+        let dieTwoValue = Math.floor(Math.random()* images.length);
+
+        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+        document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
+        document.querySelector("#total").innerHTML = "Your roll is " + ( (dieOneValue +1) + (dieTwoValue + 1));
+    },
+    1000
+    );    
+};
+
+rollButton.addEventListener("click" , roll);
+roll();
